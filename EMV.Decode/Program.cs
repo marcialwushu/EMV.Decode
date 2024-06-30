415e626dc05518a82556b870fb1964d85a8fb2cd
cleilson pereira <marcialwushu@hotmail.com>
2024-06-30 02:58:15 -0300

    Adicionar arquivos de projeto.

---
+++ EMV.Decode/Program.cs
@@ -0,0 +1,94 @@
+﻿
+
+namespace EMV.Decode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o código Pix (copia e cola):");
+
+            string pixCode = Console.ReadLine();
+
+            try
+            {
+                var decodedEMV = DecodeEMV(pixCode);
+                Console.WriteLine("======================================");
+                PrintEMVStructure(decodedEMV);
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message + ex.StackTrace;
+
+                Console.WriteLine(message);
+            }
+            
+        }
+
+        static Dictionary<string, string> DecodeEMV(string pixCode)
+        {
+            Dictionary<string, string> emvData = new Dictionary<string, string>();
+
+            try
+            {
+                int index = 0;
+                while (index < pixCode.Length)
+                {
+                    string tag = pixCode.Substring(index, 2);
+                    index += 2;
+                    int length = int.Parse(pixCode.Substring(index, 2));
+                    index += 2;
+                    string value = pixCode.Substring(index, length);
+                    index += length;
+
+                    emvData[tag] = value;
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message + ex.StackTrace; 
+
+                Console.WriteLine(message);
+            }
+            return emvData;
+        }
+
+        static void PrintEMVStructure(Dictionary<string, string> emvData)
+        {
+            Dictionary<string, string> emvTags = new Dictionary<string, string>
+            {
+                {"00", "Payload Format Indicator"},
+                {"01", "Point of Initiation Method"},
+                {"26", "Merchant Account Information"},
+                {"52", "Merchant Category Code"},
+                {"53", "Transaction Currency"},
+                {"54", "Transaction Amount"},
+                {"58", "Country Code"},
+                {"59", "Merchant Name"},
+                {"60", "Merchant City"},
+                {"62", "Additional Data Field Template"},
+                {"63", "CRC"}
+            };
+
+            try
+            {
+                foreach (var tag in emvData)
+                {
+                    string tagName = emvTags.ContainsKey(tag.Key) ? emvTags[tag.Key] : "Unknown Tag";
+                    //Console.WriteLine($"{tagName} ({tag.Key}): {tag.Value}");
+                    Console.WriteLine($"({tag.Key}) {tagName} : {tag.Value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            
+        }
+    }
+}
+
+
---
+++ JwtDecoder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+
+namespace JwtDecoder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o JWT:");
+            string jwt = Console.ReadLine();
+
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(jwt) as JwtSecurityToken;
+
+            if (jsonToken != null)
+            {
+                var header = jsonToken.Header;
+                var payload = jsonToken.Payload;
+                var claims = jsonToken.Claims;
+
+
+                string headerJson = JsonSerializer.Serialize(header, new JsonSerializerOptions { WriteIndented = true });
+                string payloadJson = JsonSerializer.Serialize(payload, new JsonSerializerOptions { WriteIndented = true });
+                string claimsJson = JsonSerializer.Serialize(claims, new  JsonSerializerOptions { WriteIndented = true });
+
+                Console.WriteLine("Header JSON:");
+                Console.WriteLine(headerJson);
+
+                Console.WriteLine("Payload JSON:");
+                Console.WriteLine(payloadJson);
+
+                Console.WriteLine("Claims JSON:");
+                Console.WriteLine(claimsJson);
+
+                foreach (var tag in claims)
+                {
+                    var vemvData = tag;
+                    Console.WriteLine(vemvData);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Token JWT inválido.");
+            }
+        }
+    }
+}
